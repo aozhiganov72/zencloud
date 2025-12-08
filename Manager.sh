2e25ee22fc4f7f489f845185592627a7bd70244a
aozhiganov72 <aozhiganov@list.ru>
2025-12-08 08:50:54 +0300

    Update Manager.sh
    Теперь запуск монтирования и отмонтирования возможен через скрипт Manager.sh

--- Manager.sh
+++ Manager.sh
@@ -4,9 +4,12 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 CLOUDS=$(rclone listremotes 2>/dev/null | sed 's/://g' | tr '\n' '|' | sed 's/|$//')
 [ -z "$CLOUDS" ] && zenity --error --text="Нет облаков! rclone config" && exit 1
 
+ACTIONS=$(sqlite3 ${SCRIPT_DIR}/cloud.db "SELECT name FROM cloud" | tr '\n' '|')
+IFS="|" StrAcs="${ACTIONS[*]::-1}"
+
 SELECTED=$(zenity --forms --title="Выбор облака"\
     --text="Выберите действие" \
-    --combo-values="Отправить_файл_в_облако|Отправить_папку_в_облако|Отправить_файлы_в_облако|Синхронизация"\
+    --combo-values="$StrAcs"\
     --add-combo="Действие")
 [ $? -ne 0 ] && exit 0
 echo $SELECTED
@@ -15,6 +18,8 @@ if [ $? -ne 0 ] || [ -z "$SELECTED" ]; then
     exit 0
 fi
 
+# Запускаем выбранный скрипт
 SCRIPT_NAME=$(sqlite3 ${SCRIPT_DIR}/cloud.db "SELECT script FROM cloud WHERE name='$SELECTED'")
 SCRIPT_NAME=$SCRIPT_DIR/$SCRIPT_NAME
 $SCRIPT_NAME
+
